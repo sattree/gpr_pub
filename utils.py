d0d3780fca3853a0a3a0850bf02f6741523defb1
sattree <sandeep.attree@gmail.com>
2019-03-02 17:23:03 -0500

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 from itertools import chain
 import subprocess
+import json
 
 class CoreNLPServer():
     def __init__(self, classpath=None, corenlp_options=None, java_options=['-Xmx5g']):
@@ -22,3 +23,25 @@ class CoreNLPServer():
     def stop(self):
         self.popen.terminate()
         self.popen.wait()
+
+# timeout is hardcoded to 60s in nltk implementation
+def api_call(self, data, properties=None, timeout=240):
+        default_properties = {
+            'outputFormat': 'json',
+            'annotators': 'tokenize,pos,lemma,ssplit,{parser_annotator}'.format(
+                parser_annotator=self.parser_annotator
+            ),
+        }
+
+        default_properties.update(properties or {})
+
+        response = self.session.post(
+            self.url,
+            params={'properties': json.dumps(default_properties)},
+            data=data.encode(self.encoding),
+            timeout=timeout,
+        )
+
+        response.raise_for_status()
+
+        return response.json()
